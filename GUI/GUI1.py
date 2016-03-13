ecd1120ecf272e880d131157064e7ca439a75eac
Abhijeet <abhijeetdixit27@gmail.com>
2016-03-13 20:09:51 +0530

    Adding a sample quit button

--- GUI/GUI1.py
+++ GUI/GUI1.py
@@ -1,15 +1,22 @@
-from Tkinter import Tk, Frame, BOTH
+from Tkinter import Tk, BOTH
+from ttk import Frame, Button, Style
 
 class GUI(Frame):
 	def __init__(self, parent):
-		Frame.__init__(self,parent,background="white")
+		Frame.__init__(self,parent)
 		self.parent = parent
 		self.initUI()
 		pass
 
 	def initUI(self):
-		self.parent.title("Example")
+		self.parent.title("Sample Button")
+		self.style = Style()
+		self.style.theme_use("default")
+
 		self.pack(fill=BOTH, expand=1)
+
+		quitButton = Button(self, text="Quit", command=self.quit)
+		quitButton.place(x=50, y=50)
 
 
 def main():
