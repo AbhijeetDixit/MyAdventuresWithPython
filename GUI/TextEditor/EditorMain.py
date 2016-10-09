c6f98af51b154b45774e3c5a68545d7aca840646
Abhijeet <abhijeetdixit27@gmail.com>
2016-10-09 18:27:59 +0530

    Adding scrollbars and status bar. Currently with static text

--- GUI/TextEditor/EditorMain.py
+++ GUI/TextEditor/EditorMain.py
@@ -12,22 +12,43 @@ class Editor(object):
 		self.rootWindow.tk.eval('package require Tix')
 
 	def addWidgetsNRun(self):
+		textVar = Tkinter.StringVar()
 		# Adding a mainFrame that contains all other widgets
 		mainFrame = Tkinter.Frame(self.rootWindow)
 		mainFrame.pack()
 
 		# Adding a top and bottom frame for menu and text area
 		# respectively
-		bottomFrame = Tkinter.Frame(self.rootWindow)
-		bottomFrame.pack(side=Tkinter.BOTTOM)
+		middleFrame = Tkinter.Frame(self.rootWindow)
+		middleFrame.pack()
 
 		topFrame = Tkinter.Frame(self.rootWindow)
 		topFrame.pack(side=Tkinter.TOP)
 
+		bottomFrame = Tkinter.Frame(self.rootWindow, bd=1)
+		bottomFrame.pack(side=Tkinter.BOTTOM)
+
+		# Adding scrollbars
+		VScrollBar = Tkinter.Scrollbar(middleFrame)
+		VScrollBar.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
+
+		HScrollBar = Tkinter.Scrollbar(middleFrame,orient=Tkinter.HORIZONTAL)
+		HScrollBar.pack(side=Tkinter.BOTTOM, fill=Tkinter.X)
+
 		# Adding a text area where all the text goes
-		text = Tkinter.Text(bottomFrame,undo=True)
+		text = Tkinter.Text(middleFrame,undo=True,bd=1)
 		text.insert(Tkinter.INSERT,"")
 		text.pack()
+
+		# Attaching scrollbars to Text
+		text.config(yscrollcommand=VScrollBar.set)
+		VScrollBar.config(command=text.yview)
+		text.config(xscrollcommand=HScrollBar.set)
+		HScrollBar.config(command=text.xview)
+
+		statusLabel = Tkinter.Label(bottomFrame, bd=1, anchor=Tkinter.E, textvariable=textVar, font=('ariel',12,'normal'))
+		statusLabel.grid(row=0,column=0,sticky=Tkinter.W)
+		textVar.set('Ln 0,Col 0')
 
 		# Instantiating a createmenu class and creating menu
 		createmenu = createMenu()
